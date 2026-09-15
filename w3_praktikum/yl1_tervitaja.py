636c279cbac32fb0248b96d1fabcd641545ac461
HELLRYZEN\helar <hniilo@gmail.com>
2026-09-15 12:36:57 +0300

    vanuse kontroll, andmed sõnastikus

--- w3_praktikum/yl1_tervitaja.py
+++ w3_praktikum/yl1_tervitaja.py
@@ -1,6 +1,17 @@
-# Küsi kasutaja nimi ja sünniaasta
+def kontrollitav(vanuse_kontroll):
+    vanused = {14: "süüdiv",
+               16: "KOV",
+               18: ["RK", "EP", "ÕLU"],
+               21: "kasiino",
+               24: "A-kat",
+               40: "President"
+               }
+    for vanus_nimekirjast in vanused.keys():
+        if vanuse_kontroll >= vanus_nimekirjast:
+            print(vanused[vanus_nimekirjast])
+    
 nimi = input("Mis su nimi on?")
 sünniaasta = int(input("Mis su sünniaasta on?"))
 vanus = 2026 - sünniaasta
-print("Tere,", nimi ,"Sa oled umbes", vanus," aastat vana.")
-print(f"Tere, {nimi}! Sa oled umbes {vanus} aastat vana.")
+
+kontrollitav(vanus)
