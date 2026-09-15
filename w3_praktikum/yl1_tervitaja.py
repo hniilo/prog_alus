b3342615f78236ed723ab65eb351b00b65c7766f
HELLRYZEN\helar <hniilo@gmail.com>
2026-09-15 10:36:09 +0300

    tervitaja ülesanne. Sisendi lugemine, muutujate deklareerimne, tüübi muudatus, printisime väljundi. Näide f-stringi kasutamise kohta

---
+++ w3_praktikum/yl1_tervitaja.py
@@ -0,0 +1,6 @@
+# Küsi kasutaja nimi ja sünniaasta
+nimi = input("Mis su nimi on?")
+sünniaasta = int(input("Mis su sünniaasta on?"))
+vanus = 2026 - sünniaasta
+print("Tere,", nimi ,"Sa oled umbes", vanus," aastat vana.")
+print(f"Tere, {nimi}! Sa oled umbes {vanus} aastat vana.")
